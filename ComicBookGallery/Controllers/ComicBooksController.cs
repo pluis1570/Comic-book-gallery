2ceecb0f7291980aa0d2d15d0924f490cffe5a01
Laurent Luypaert <laur.luyp@hotmail.com>
2019-06-03 13:33:15 +0200

    Updated comic book Detail view to be strongly typed

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -10,26 +11,24 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spiderman";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
 
-            return View();
-            //if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
-            //{
-            //    return new RedirectResult("/");
-            //}
-            //return new ContentResult()
-            //{ Content = "Hello from the ComicBooks Controller!" };
-            //// return "Hello from the ComicBooks Controller!";
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spiderman",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist(){Name = "Dan Slott",Role= "Script"},
+                    new Artist(){Name = "Humberto Ramos",Role= "Pencils"},
+                    new Artist(){Name = "Victor Olazaba",Role= "Inks"},
+                    new Artist(){Name = "Edgar Delgado",Role= "Colors"},
+                    new Artist(){Name = "Chris Eliopoulos",Role= "Letters"},
+                }
+        };
+
+            return View(comicBook);
+            
         }
     }
 }
